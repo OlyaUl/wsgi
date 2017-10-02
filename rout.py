c19260fb80741ec2d4f5afee71fc7c7a7c8b4b93
Olya <johndoe@example.com>
2017-10-02 14:59:14 +0300

    route

--- rout.py
+++ rout.py
@@ -3,21 +3,33 @@
 from werkzeug.routing import Map, Rule
 from werkzeug.wrappers import Request, Response
 
+
+def index(request):
+    return Response('index page')
+
+
+def hash(request, hash):
+    return Response('hash = %s' % hash)
+
 map_rule = Map([
     Rule('/', endpoint='index'),
     Rule('/<hash>', endpoint='hash'),
 ])
 
+views = {
+    'index': index,
+    'hash': hash
+}
 
 def app(environ, start_response):
     urls = map_rule.bind_to_environ(environ)
     try:
         endpoint, args = urls.match()
     except Exception:
-        return [b'Error']
-    print(endpoint)
-    print(args)
-    resp = Response('OK')
+        start_response('404 Not Found')
+        return [b'rule not found']
+
+    resp = views[endpoint](Request(environ), **args)
     return resp(environ, start_response)
 
 if __name__ == '__main__':
