3e56ee4c0d791597e252daf363c8919f096034f3
Olya <johndoe@example.com>
2017-10-02 14:27:54 +0300

    werk1

--- main.py
+++ main.py
@@ -3,7 +3,18 @@
 from werkzeug.wrappers import Request, Response
 
 
-def app(environ, start_response):
+@Request.application
+def app_dec(request):
+    res = Response('hello')
+    res.status = '200 OK'
+    res.status_code = 400
+    #res.add_cookie()
+    print(request.args)
+    print(request.args.getlist('a'))  # http://localhost:8000/?a=4&f=5 -> ['4']
+    return res
+    
+    #return Response("Hello")
+'''def app(environ, start_response):
     request = Request(environ)
     print(request.args)
     print(request.form)
@@ -12,8 +23,8 @@ def app(environ, start_response):
     print(request.method)
     print(request.headers)
     response = Response('hello', mimetype='text/plain')
-    return response(environ, start_response)
+    return response(environ, start_response)'''
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
-    run_simple('localhost', 8000, app)
+    run_simple('localhost', 8000, app_dec)
